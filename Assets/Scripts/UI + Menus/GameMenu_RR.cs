594db94ff41e4dc33f736d78556d771df9d50e2b
raees ellam <ELL20001480@wakefield.ac.uk>
2022-05-12 14:59:04 +0100

    Menus COMPLETED + Lighting fixes (V2.6

--- Assets/Scripts/UI + Menus/GameMenu_RR.cs
+++ Assets/Scripts/UI + Menus/GameMenu_RR.cs
@@ -6,27 +6,36 @@ using UnityEngine.UI;
 
 public class GameMenu_RR : MonoBehaviour
 {
-    public GameObject mainMenu;
-    public GameObject settingsMenu;
+    public GameObject gameUI;
     public GameObject gameMenu;
 
+    public GameObject carPart1, carPart2, carPart3;
+    public GameObject gameStart;
+
+    public void Play()
+    {
+        carPart1.SetActive(true);
+        carPart2.SetActive(true);
+        carPart3.SetActive(true);
+
+        gameUI.SetActive(true);
+        gameMenu.SetActive(false);
+    }
     public void GameResults()
     {
-        SceneManager.LoadScene("Menus"); //loads the Menus scene
-        mainMenu.SetActive(false);
-        settingsMenu.SetActive(false);
+        carPart1.SetActive(false);
+        carPart2.SetActive(false);
+        carPart3.SetActive(false);
+
+        gameUI.SetActive(false);
         gameMenu.SetActive(true);
-    }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+        carPart1.transform.position = gameStart.transform.position;
+        carPart2.transform.position = gameStart.transform.position;
+        carPart3.transform.position = gameStart.transform.position;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        carPart1.transform.Rotate(0f, 90f, 0f);
+        carPart2.transform.Rotate(0f, 90f, 0f);
+        carPart3.transform.Rotate(0f, 90f, 0f);
     }
 }

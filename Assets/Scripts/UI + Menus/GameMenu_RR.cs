c1e1267b93f4597f25d3bc0f44d4b729357c6d35
raees ellam <ellamraees6@gmail.com>
2022-05-14 17:51:44 +0100

    Game Difficulty added (V2.8)

--- Assets/Scripts/UI + Menus/GameMenu_RR.cs
+++ Assets/Scripts/UI + Menus/GameMenu_RR.cs
@@ -13,6 +13,8 @@ public class GameMenu_RR : MonoBehaviour
     public GameObject gameStart;
 
     public ReturnHome_RR returnHome_RR;
+
+    public GameObject policeCar1, policeCar2, policeCar3, policeCar4, policeCar5, policeCar6, policeCar7, policeCar8;
     public void Play()
     {
         carPart1.SetActive(true);
@@ -40,5 +42,38 @@ public class GameMenu_RR : MonoBehaviour
         carPart1.transform.Rotate(0f, 90f, 0f);
         carPart2.transform.Rotate(0f, 90f, 0f);
         carPart3.transform.Rotate(0f, 90f, 0f);
+    }
+    public void GameModeEasy()
+    {
+        policeCar1.SetActive(true);
+        policeCar2.SetActive(true);
+        policeCar3.SetActive(false);
+        policeCar4.SetActive(false);
+        policeCar5.SetActive(false);
+        policeCar6.SetActive(false);
+        policeCar7.SetActive(false);
+        policeCar8.SetActive(false);
+    }
+    public void GameModeMedium()
+    {
+        policeCar1.SetActive(true);
+        policeCar2.SetActive(true);
+        policeCar3.SetActive(true);
+        policeCar4.SetActive(true);
+        policeCar5.SetActive(true);
+        policeCar6.SetActive(false);
+        policeCar7.SetActive(false);
+        policeCar8.SetActive(false);
+    }
+    public void GameModeHard()
+    {
+        policeCar1.SetActive(true);
+        policeCar2.SetActive(true);
+        policeCar3.SetActive(true);
+        policeCar4.SetActive(true);
+        policeCar5.SetActive(true);
+        policeCar6.SetActive(true);
+        policeCar7.SetActive(true);
+        policeCar8.SetActive(true);
     }
 }
